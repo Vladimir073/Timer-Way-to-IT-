c092d414c2cec57581e62ab4f53411120b5aa169
Vladimir <99907985+Vladimir073@users.noreply.github.com>
2022-11-05 12:40:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const columns = document.querySelectorAll('.column .number');
+
+const countdownDate = new Date (2022, 01, 14, 16, 00, 00).getTime();
+
+console.log(countdownDate)
+
+function getCountdownTime() {
+const now = new Date().getTime();
+const distance = now - countdownDate;
+
+
+// 1c = 1000мс
+// 1м = 60с
+// 1ч = 60м
+// 1д = 24ч
+
+const oneDay = 24 * 60 * 60 * 1000;
+const oneHour = 60 * 60 * 1000;
+const oneMinute = 60 * 1000;
+
+let days = Math.floor(distance/oneDay);
+let hours = Math.floor((distance % oneDay) / oneHour);
+let minuts = Math.floor((distance % oneHour) / oneMinute);
+let seconds = Math.floor((distance % oneMinute) / 1000);
+
+const values = [days, hours, minuts, seconds];
+
+columns.forEach((item, index) => {
+    item.textContent = values[index];
+})
+}
+
+getCountdownTime();
+
+setInterval(getCountdownTime, 1000);
